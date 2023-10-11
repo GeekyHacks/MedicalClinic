1036ffb75e75c742d453b7f8f1f96cc51e9fdc7b
GeekyHacks <geekyhacks22@gmail.com>
2023-10-11 21:19:41 +0300

    Edit readme and correct the typos

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,7 +6,6 @@ CREATE TABLE patients(
     date_Of_birth DATE,
     PRIMARY KEY (id)
 );
-
 -- create medical_histories table
 CREATE TABLE medical_histories(
     id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
@@ -14,9 +13,8 @@ CREATE TABLE medical_histories(
     patient_id INT,
     status VARCHAR(100),
     PRIMARY KEY (id),
-    FOREIGN KEY (patient_id) REFERENCES patients (id);
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
 );
-
 -- create treatments table
 CREATE TABLE treatments(
     id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
@@ -24,7 +22,6 @@ CREATE TABLE treatments(
     name VARCHAR(120),
     PRIMARY KEY (id)
 );
-
 -- create invoice_items table
 CREATE TABLE invoice_items(
     id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
@@ -37,20 +34,18 @@ CREATE TABLE invoice_items(
     FOREIGN KEY (treatment_id) REFERENCES treatments (id),
     PRIMARY KEY (id)
 );
-
 -- create invoice_items table
 CREATE TABLE invoices(
     id INT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
-    total_amount DECIMAL,
+    total_amount FLOAT,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
-    medical_history_id,
+    medical_history_id INT,
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
     PRIMARY KEY (id)
 );
-
 -- create join table medical_histories-treatments table
-CREATE TABLE medical_histories-treatments(
+CREATE TABLE medical_histories_treatments(
     medical_histories_id INT,
     treatments_id INT,
     FOREIGN KEY (medical_histories_id) REFERENCES medical_histories (id),
